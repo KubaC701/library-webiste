ce66ebd04f536b90fe8a835c88a2dff4508e53a0
Jakub Ciepły <jcieply@edu.cdv.pl>
2023-03-12 16:41:29 +0100

    Add login page

---
+++ src/components/login/Login.jsx
@@ -0,0 +1,55 @@
+import "./login.css";
+
+const Login = () => {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log("submit");
+  };
+
+  return (
+    <main className="container">
+      <div className="background-wrapper--desktop">
+        <img
+          src="/background.png"
+          alt=""
+          className="background--desktop"
+        />
+      </div>
+      <div className="form-wrapper">
+        <div>
+          <img
+            src="/logo.png"
+            alt="Logo Motigu"
+            className="logo"
+          />
+          <h1 className="title">Motigu</h1>
+          <form onSubmit={handleSubmit} className="form">
+            <label className="label" htmlFor="login">
+              Login
+            </label>
+            <input
+              className="input input-login"
+              type="text"
+              name="login"
+              id="login"
+            />
+            <label className="label" htmlFor="password">
+              Password
+            </label>
+            <input
+              className="input"
+              type="password"
+              name="password"
+              id="password"
+            />
+            <button type="submit" className="button">
+              Login
+            </button>
+          </form>
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default Login;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import "./index.css";
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Login from "./components/login/Login";
+
+const router = createBrowserRouter([
+  {
+    path: "/login",
+    element: <Login />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
