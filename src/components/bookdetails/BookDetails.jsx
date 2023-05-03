04105474aaaa3dd2d0e35ee57ff86df0ee1906fb
Patrycja Bednarz <p.m.bednarz@gmail.com>
2023-05-03 20:28:16 +0200

    book

---
+++ src/components/bookdetails/BookDetails.jsx
@@ -0,0 +1,26 @@
+import "./bookdetails.css";
+
+
+
+  return (
+    <main className="container">
+      <div className="background-wrapper--desktop">
+        <img
+          src="/background.png"
+          alt=""
+          className="background--desktop"
+        />
+      </div>
+      <div className="form-wrapper">
+ 
+            <button type="submit" className="button">
+              Login
+            </button>
+        
+       
+      </div>
+    </main>
+  );
+
+
+export default BookDetails;
