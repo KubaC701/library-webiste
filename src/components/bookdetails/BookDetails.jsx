93bef07b352a4e847f6ecf3788f58201c0a5f507
Patrycja Bednarz <p.m.bednarz@gmail.com>
2023-05-04 04:58:18 +0200

    zawartosc

--- src/components/bookdetails/BookDetails.jsx
+++ src/components/bookdetails/BookDetails.jsx
@@ -1,31 +1,63 @@
 import "./bookdetails.css";
 
 const BookDetails = () => {
-
-    const handleSubmit = (event) => {
+  const handleSubmit = (event) => {
       event.preventDefault();
       console.log("submit");
     };
   
-    return 
-    (
+    return (
     <main className="container">
-      <div className="background-wrapper--desktop">
-        <img
-          src="/background.png"
-          alt=""
-          className="background--desktop"
-        />
-      </div>
-      <div className="form-wrapper">
- 
-            <button type="subscribe" className="button">
-              Subscribe
-            </button>
+
+  
+
+            <div className="divbook">
+
+
+
+            <section>
+            <h1>Był sobie Gdańsk</h1>
+            <h2>Wojciech Duda, Grzegorz Fortuna, Donald Tusk</h2>
+            </section>
+
+
+
+
+            <img
+                  src="/book0001.jpg"
+                  alt="był sobie gdańsk"
+                  className="logo"
+                />
+
+            
+          
+          
+            <div className="form-wrapper">
+
+
+
+            <form onSubmit={handleSubmit} className="form">
+         
+
+            <button type="submit" className="button">Reserve</button>
+         
+            <span className="shortinfo">  After reservation you’ll have 3 days to pick up you book from the library after this time your reservation will be cancelled</span>
+         
+       <h3>Description</h3> 
+            <div>
+            <p>Na album "Był sobie Gdańsk" złożyły się zdjęcia z wielu zbiorów archiwalnych, muzealnych i prywatnych. Przejrzeliśmy ich tysiące. Naszym zamiarem było pokazanie przede wszystkim tych fragmentów miasta, które zniknęły bezpowrotnie lub zmieniły swój charakter.
+            </p>
+            </div>
+
+          </form>
+            </div>
+
+   
+
       </div>
     </main>
   );
-  }
+  };
 
 export default BookDetails;
 
