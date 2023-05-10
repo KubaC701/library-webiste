40f53fb009cd22a5b0f362f04ae1326876f418fe
Patrycja Bednarz <p.m.bednarz@gmail.com>
2023-05-10 04:34:44 +0200

    new user site

--- src/components/booksearch/BookSearch.jsx
+++ src/components/booksearch/BookSearch.jsx
@@ -8,20 +8,54 @@ const BookSearch = () => {
   
     return (
     <main className="container">
+
+
+
+
+
       <div className="form-wrapper">
-        <section>
-          <h1>Był sobie Gdańsk</h1>
-          <h2 className="middlecolor">Wojciech Duda, Grzegorz Fortuna, Donald Tusk</h2>
-        </section>
-        <img src="/book0001.jpg" alt="był sobie gdańsk" className="logo frame"/>
-        <button type="submit" className="button">Reserve</button>
-        <span>  After reservation you’ll have 3 days to pick up you book from the library after this time your reservation will be cancelled</span>
-        <h2>Description</h2> 
+
+        <input
+        className="input"
+        type="text"
+        name="login"
+        id="login"
+        value="Search"
+        />
+
         <div className="divdescription">
-          <p>Na album "Był sobie Gdańsk" złożyły się zdjęcia z wielu zbiorów archiwalnych, muzealnych i prywatnych. Przejrzeliśmy ich tysiące. Naszym zamiarem było pokazanie przede wszystkim tych fragmentów miasta, które zniknęły bezpowrotnie lub zmieniły swój charakter.</p>
+          <img src="/book0001.jpg" alt="był sobie gdańsk" className="logo frame"/>
+          <section>
+            <h1>Był sobie Gdańsk</h1>
+            <h2 className="middlecolor">Wojciech Duda, Grzegorz Fortuna, Donald Tusk</h2>
+          </section>
+          <button type="submit" className="button">Reserve</button> 
         </div>
+
+        <div className="divdescription">
+          <img src="/book0001.jpg" alt="był sobie gdańsk" className="logo frame"/>
+          <section>
+            <h1>Był sobie Gdańsk</h1>
+            <h2 className="middlecolor">Wojciech Duda, Grzegorz Fortuna, Donald Tusk</h2>
+          </section>
+          <button type="submit" className="button">Reserve</button> 
+        </div>
+
+
+          
+        <span>  After reservation you’ll have 3 days to pick up you book from the library after this time your reservation will be cancelled</span>
       </div>
     </main>
+
+
+
+
+
+
+
+
+
+
   );
   };
 
