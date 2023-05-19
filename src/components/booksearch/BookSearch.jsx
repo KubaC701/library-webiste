49bade37243cfdf015786f4b600d2eb26cd6ea53
Jakub Ciepły <jcieply@edu.cdv.pl>
2023-05-19 18:15:45 +0200

    Use real data to render list of books

--- src/components/booksearch/BookSearch.jsx
+++ src/components/booksearch/BookSearch.jsx
@@ -1,98 +1,33 @@
-import "./booksearch.css";
+import './booksearch.css';
+import books from '../../data/books.json';
 
 const BookSearch = () => {
-  const handleSubmit = (event) => {
-      event.preventDefault();
-      console.log("submit");
-    };
-  
-    return (
+  return (
     <main className="container">
-
-      <div><h1>logo's space</h1></div>
-      <input
-        className="input"
-        type="text"
-        name="login"
-        id="login"
-        placeholder="Search"
-        />
-
+      <div>
+        <h1>logo&apos;s space</h1>
+      </div>
+      <input className="input" type="text" name="login" id="login" placeholder="Search" />
       <div className="tab">
-
-        <div className="item">
-          <img src="/book0001.jpg" alt="był sobie gdańsk" className="logo frame"/>
-          <section>
-            <h1>Był sobie Gdańsk</h1>
-            <h2 className="middlecolor">Wojciech Duda, Grzegorz Fortuna, Donald Tusk</h2>
-          </section>
-          <button type="submit" className="button">Reserve</button> 
-        </div>
-
-        <div className="item">
-          <img src="/book0001.jpg" alt="był sobie gdańsk" className="logo frame"/>
-          <section>
-            <h1>Był sobie Gdańsk</h1>
-            <h2 className="middlecolor">Wojciech Duda, Grzegorz Fortuna, Donald Tusk</h2>
-          </section>
-          <button type="submit" className="button">Reserve</button> 
-        </div>
-
-        <div className="item">
-          <img src="/book0001.jpg" alt="był sobie gdańsk" className="logo frame"/>
-          <section>
-            <h1>Był sobie Gdańsk</h1>
-            <h2 className="middlecolor">Wojciech Duda, Grzegorz Fortuna, Donald Tusk</h2>
-          </section>
-          <button type="submit" className="button">Reserve</button> 
-        </div>
-
-        <div className="item">
-          <img src="/book0001.jpg" alt="był sobie gdańsk" className="logo frame"/>
-          <section>
-            <h1>Był sobie Gdańsk</h1>
-            <h2 className="middlecolor">Wojciech Duda, Grzegorz Fortuna, Donald Tusk</h2>
-          </section>
-          <button type="submit" className="button">Reserve</button> 
-        </div>
-
-        <div className="item">
-          <img src="/book0001.jpg" alt="był sobie gdańsk" className="logo frame"/>
-          <section>
-            <h1>Był sobie Gdańsk</h1>
-            <h2 className="middlecolor">Wojciech Duda, Grzegorz Fortuna, Donald Tusk</h2>
-          </section>
-          <button type="submit" className="button">Reserve</button> 
-        </div>
-
-
-        <div className="item">
-          <img src="/book0001.jpg" alt="był sobie gdańsk" className="logo frame"/>
-          <section>
-            <h1>Był sobie Gdańsk</h1>
-            <h2 className="middlecolor">Wojciech Duda, Grzegorz Fortuna, Donald Tusk</h2>
-          </section>
-          <button type="submit" className="button">Reserve</button> 
-        </div>
-
-
-
-        </div>
-          
-        <span>  After reservation you’ll have 3 days to pick up you book from the library after this time your reservation will be cancelled</span>
-      
+        {books.map((book) => (
+          <div className="item" key={`${book.title} ${book.author}`}>
+            <img src={book.thumbnail} alt={book.title} className="logo frame" />
+            <section>
+              <h1>{book.title}</h1>
+              <h2 className="middlecolor">{book.author}</h2>
+            </section>
+            <button type="submit" className="button">
+              Reserve
+            </button>
+          </div>
+        ))}
+      </div>
+      <span>
+        After reservation you’ll have 3 days to pick up you book from the library after this time
+        your reservation will be cancelled
+      </span>
     </main>
-
-
-
-
-
-
-
-
-
-
   );
-  };
+};
 
 export default BookSearch;
