1c6f1321d5333a50fb1d4728c8c1119517966c3d
pyccia <96018262+pyccia@users.noreply.github.com>
2023-05-20 07:39:21 +0200

    I need main menu

--- src/components/booksearch/BookSearch.jsx
+++ src/components/booksearch/BookSearch.jsx
@@ -3,22 +3,53 @@ import books from '../../data/books.json';
 
 const BookSearch = () => {
   return (
-    <main className="container">
-      <div>
+   
+   
+   
+   
+   <main  className="booksearch__backgroundandscroll">
+
+
+
+
+
+
+
+    <div className="booksearch">
+
+      <div >
         <h1>logo&apos;s space</h1>
       </div>
-      <input className="input" type="text" name="login" id="login" placeholder="Search" />
-      <div className="tab">
+      <div className="booksearch__searcher">
+        <input className="booksearch__input" type="text" name="searcher" id="searcher" placeholder="Book's title" />
+
+
+        <button type="submit" className="booksearch__searchbutton">
+              Search
+        </button>
+
+      </div>
+
+
+
+
+
+
+      <div className="booksearch__items">
         {books.map((book) => (
-          <div className="item" key={`${book.title} ${book.author}`}>
-            <img src={book.thumbnail} alt={book.title} className="logo frame" />
-            <section>
+          <div className="booksearch__item" type="submit" key={`${book.title} ${book.author}`}>
+            <div>
+            <img src={book.thumbnail} alt={book.title} className="booksearch__img" />
+            </div>
+            <div>
               <h1>{book.title}</h1>
               <h2 className="middlecolor">{book.author}</h2>
-            </section>
-            <button type="submit" className="button">
+            </div>
+            <div>
+            <button type="submit" className="booksearch__button">
               Reserve
             </button>
+            </div>
           </div>
         ))}
       </div>
@@ -26,6 +57,7 @@ const BookSearch = () => {
         After reservation you’ll have 3 days to pick up you book from the library after this time
         your reservation will be cancelled
       </span>
+      </div>
     </main>
   );
 };
