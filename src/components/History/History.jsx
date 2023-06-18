f7077f5ec5dfacf8b3c985592cb0fc9d081edeeb
pyccia <96018262+pyccia@users.noreply.github.com>
2023-06-18 16:58:10 +0200

    dosplay history details

--- src/components/History/History.jsx
+++ src/components/History/History.jsx
@@ -1,8 +1,25 @@
 import './history.css';
-import usershistory from '../../data/usershistory.json';
+import historyData from '../../data/usershistory.json';
+import useAuthContext from '../../contexts/AuthContext';
 import Layout from '../Layout/Layout';
+import books from '../../data/books.json';
 
 const History = () => {
+  // keep the information about logged in user
+  const { user } = useAuthContext();
+  const userHistory = historyData.find((historyItem) => historyItem.email === user.email);
+
+  const booksToDisplay = userHistory.books.map((book) => {
+    const matchedBook = books.find((bookData) => bookData.id === book.id);
+    if (matchedBook) {
+      return {
+        ...book,
+        ...matchedBook,
+      };
+    }
+    return book;
+  });
+
   return (
     <Layout className="history__container">
       <div className="history__form-wrapper">
@@ -12,25 +29,27 @@ const History = () => {
         </section>
 
         <div className="history__list">
-          {usershistory.map((borrowed) => (
-            <div className="history__item" key={`${borrowed.from} ${borrowed.to} ${borrowed.to}`}>
+          {booksToDisplay.map((book) => (
+            <div className="history__item" key={book.id}>
               <div className="history_cover">
-                <img src={borrowed.mail} alt={borrowed.from} className="history__cover" />
+                <img src={book.thumbnail} alt={book.title} className="history__cover" />
               </div>
               <div className="history_cover">
                 <div className="history__line">
                   <h3 className="history__h3">Title:</h3>
-                  <p className="history__p">{borrowed.to}</p>
+                  <p className="history__p">{book.title}</p>
                 </div>
                 <div className="history__line">
                   <h3 className="history__h3">Author:</h3>
-                  <p className="history__p"> {borrowed.mail}</p>
+                  <p className="history__p"> {book.author}</p>
                 </div>
                 <div className="history__line">
                   <h3 className="history__h3">From:</h3>
+                  <p className="history__p">{book.from}</p>
                 </div>
                 <div className="history__line">
                   <h3 className="history__h3">To:</h3>
+                  <p className="history__p">{book.to}</p>
                 </div>
               </div>
             </div>
