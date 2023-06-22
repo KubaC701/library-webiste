20db4c38db7dc9329b54122c2ca1d1d7af016c4f
Diana L <dlehmann@edu.cdv.pl>
2023-06-22 20:57:42 +0200

    last changes

--- src/components/ManageUsers/UserSearch.jsx
+++ src/components/ManageUsers/UserSearch.jsx
@@ -1,19 +1,19 @@
 const UserSearch = () => {
   return (
-    <div className="usersearch__container">
-      <div>
-        <label className="usersearch__label">Enter user email:</label>
-      </div>
-      <div className="usersearch__search">
+    <div className="user-search__container">
+      <label className="user-search__label" htmlFor="search">
+        Enter user email:
+      </label>
+      <div className="user-search__search">
         <input
-          className="usersearch__input"
+          className="user-search__input"
           type="text"
           name="search"
           id="search"
           placeholder="example@lib.com"
         />
       </div>
-      <button type="active" className="usersearch__adduserbutton">
+      <button type="button" className="user-search__adduserbutton">
         Add user
       </button>
     </div>
