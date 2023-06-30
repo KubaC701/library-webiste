275b0d81dc2086cda386d36a50cc621fbe745616
Diana L <dlehmann@edu.cdv.pl>
2023-06-30 23:18:21 +0200

    Description CSS changes according to Figma

--- src/components/EditBook/EditBookHistory.jsx
+++ src/components/EditBook/EditBookHistory.jsx
@@ -6,11 +6,9 @@ const EditBookHistory = () => {
       </div>
       <div className="edit-book-history__info">
         <ul className="edit-book-history__list">
-          <li> 12-03-2023 Joe borrowed a book,</li>
-          <li>22-03-2023 Joe returned a book, </li>
-          <li>08-05-2023 Alice borrowed a book,</li>
-          <li>17-06-2023 Alice returned a book,</li>
-          <li> 28-06-2023 Rob borrowed a book </li>
+          <li>12-03-2023 Joe borrowed a book</li>
+          <li>22-03-2023 Joe returned a book</li>
+          <li>08-05-2023 Alice borrowed a book</li>
         </ul>
       </div>
     </div>
