47a7b76df37ffdb138a3b24807cfe731cb8de224
Jakub Ciepły <jcieply@edu.cdv.pl>
2023-07-02 09:10:29 +0200

    Use Input and Button components on login page

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -2,16 +2,32 @@ import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import useAuthContext from '../../contexts/AuthContext';
 import './login.css';
+import Input from '../Input/Input';
+import Button from '../Button/Button';
 
 const Login = () => {
   const navigate = useNavigate();
   const [isLoginError, setIsLoginError] = useState(false);
+  const [isEmailError, setIsEmailError] = useState(false);
+  const [isPasswordError, setIsPasswordError] = useState(false);
   const { signIn } = useAuthContext();
 
   const handleSubmit = async (event) => {
     event.preventDefault();
 
     const { email, password } = event.target.elements;
+
+    let isFilled = true;
+    if (email.value === '') {
+      setIsEmailError(true);
+      isFilled = false;
+    }
+    if (password.value === '') {
+      setIsPasswordError(true);
+      isFilled = false;
+    }
+    if (!isFilled) return;
+
     const isValidUser = signIn({ email: email.value, password: password.value });
 
     if (isValidUser) {
@@ -35,17 +51,27 @@ const Login = () => {
           <img src="/logo.png" alt="Logo Motigu" className="login__logo" />
           <h1 className="login__title">Motigu</h1>
           <form onSubmit={handleSubmit} className="login__form">
-            <label className="login__label" htmlFor="email">
-              Email
-            </label>
-            <input className="login__input input-login" type="text" name="email" id="email" />
-            <label className="login__label" htmlFor="password">
-              Password
-            </label>
-            <input className="login__input" type="password" name="password" id="password" />
-            <button type="submit" className="login__button">
+            <Input
+              className="login__input login__input--email"
+              type="text"
+              name="email"
+              id="email"
+              label="Email"
+              error={isEmailError && 'Email is required'}
+              onChange={() => setIsEmailError(false)}
+            />
+            <Input
+              className="login__input"
+              type="password"
+              name="password"
+              id="password"
+              label="Password"
+              error={isPasswordError && 'Password is required'}
+              onChange={() => setIsPasswordError(false)}
+            />
+            <Button type="submit" className="login__button" size="big">
               Login
-            </button>
+            </Button>
           </form>
         </div>
       </div>
