e8d3380a8a545c7b6109cbe47a5ae38f836e92f8
Jakub Ciepły <jcieply@edu.cdv.pl>
2023-07-05 19:12:16 +0200

    Restore book details

---
+++ src/components/BookDetails/BookDetails.jsx
@@ -0,0 +1,48 @@
+import './book-details.css';
+import Layout from '../Layout/Layout';
+import { Navigate, useParams } from 'react-router-dom';
+import useBooksContext from '../../contexts/BooksContext';
+import { findById } from '../../helpers/books';
+import Button from '../Button/Button';
+import useReservationButton from '../../hooks/useReservationButton';
+import { THUMBNAIL_DIMENSIONS } from '../../constants/books';
+
+const BookDetails = () => {
+  const { id } = useParams();
+  const { books } = useBooksContext();
+  const currentBook = findById(id, books);
+  const { onClick, disabled, text } = useReservationButton(currentBook);
+
+  if (!currentBook) {
+    return <Navigate to="/404" replace />;
+  }
+
+  return (
+    <Layout>
+      <h1 className="book-details__title">{currentBook.title}</h1>
+      <h2 className="book-details__author">{currentBook.author}</h2>
+      <div className="book-details__info">
+        <img
+          src={currentBook.thumbnail}
+          alt={currentBook.title}
+          className="book-details__cover"
+          width={THUMBNAIL_DIMENSIONS.WIDTH}
+          height={THUMBNAIL_DIMENSIONS.HEIGHT}
+        />
+        <div className="book-details__description-wrapper">
+          <h2 className="book-details__subheading">Description</h2>
+          <p className="book-details__description">{currentBook.description}</p>
+        </div>
+      </div>
+      <Button className="book-details__button" size="big" onClick={onClick} disabled={disabled}>
+        {text}
+      </Button>
+      <p className="book-details__reservation">
+        After reservation you’ll have 3 days to pick up you book from the library after this time
+        your reservation will be cancelled
+      </p>
+    </Layout>
+  );
+};
+
+export default BookDetails;
